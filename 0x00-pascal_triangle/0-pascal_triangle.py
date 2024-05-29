787222be7cb01ab66bfbaacd63a996d950ae3287
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-05-29 17:07:41 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,14 @@
-# A module for working with Pascal's triangle.
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
 
 def pascal_triangle(n):
+    """Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    """
     triangle = []
-    if n <= 0:
+    if type(n) is not int or n <= 0:
         return triangle
     for i in range(n):
         line = []
